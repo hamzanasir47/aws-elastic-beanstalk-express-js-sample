bfe60fb5edde8bde391a9523652dab7a3d417565
taiimrr <taimoornasir590@gmail.com>
2024-07-23 22:55:52 +0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hallo ich bin hamza, wo ist diene name?"));
+app.get("/", (req, res) =>
+  res.send("Hallo ich bin hamza, ich prufe das pipeline")
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
